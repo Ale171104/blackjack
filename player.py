650add922d8c267a33a0a1b2bc5b14447257feea
Ale171104 <valero7rinke@gmail.com>
2023-10-15 08:26:21 -0300

    test

--- player.py
+++ player.py
@@ -2,12 +2,16 @@ from deck_database import deck
 
 class Player:
     
-    
-    
+    wins = 0
+    loses = 0
+    draws = 0
+    win_ratio = 0
+    limit_exceed = False 
 
     def __init__(self):
         self.hand = []
         self.total_value = 0
+        
 
     def add_card(self):
         
@@ -19,5 +23,20 @@ class Player:
 
     def print_total_value(self):
         print(self.total_value)
-    
+
+    def return_cards(self):
+        for card in self.hand:
+            deck.cards.append(card)
+        self.hand.clear()
+        self.total_value = 0
+
+
+    def ratio(self):
+        if self.loses > 0:
+            self.win_ratio = self.wins / self.loses
+
+
+    def print_record(self):    
+        print("wins",self.wins,"|","loses",self.loses,"|","draws",self.draws,"|","win ratio",self.win_ratio)
+
         
--- player_database.py
+++ player_database.py
@@ -1,3 +1,5 @@
-#from Player import Player
+from Player import Player
 
-#player1 = Player()
+
+player1 = Player()
+house = Player()
